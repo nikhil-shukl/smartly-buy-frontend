0c144f3a35968f4ba13803bda8f231bc84fa44ea
Nikhil Shukla <nikhilshukla8652@gmail.com>
2025-04-29 18:22:53 +0530

    Fixed Vite base config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',// ← this must match the repo/project name on Vercel
-})
+  base: '/', // ✅ optional, but safer
+});
+
