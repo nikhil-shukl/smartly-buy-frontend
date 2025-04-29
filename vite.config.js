ca42dd9492bbd8e12f7e61d6051e05cecbf9f292
Nikhil Shukla <nikhilshukla8652@gmail.com>
2025-04-29 17:15:51 +0530

    fix: use relative base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'// ← this must match the repo/project name on Vercel
+  base: './',// ← this must match the repo/project name on Vercel
 })
