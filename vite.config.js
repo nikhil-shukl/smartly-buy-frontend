3b23f910ff809aa07fea5ba5a4c1998ab4290d3b
Nikhil Shukla <nikhilshukla8652@gmail.com>
2025-04-30 14:30:51 +0530

    fix: vite base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ THIS is correct for static hosting like Vercel drag-drop or GitHub Pages
+  base: '/smartly-buy-frontend/', // ✅ MATCH karo repo/project name se
 });
