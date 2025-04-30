a9b96a86c3caf22345fca7a3af62511b092b4091
Nikhil Shukla <nikhilshukla8652@gmail.com>
2025-04-30 14:44:30 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/smartly-buy-frontend/', // ✅ MATCH karo repo/project name se
+  base: '/', // ✅ MATCH karo repo/project name se
 });
