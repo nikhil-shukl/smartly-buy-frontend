80753eb747f1a7e08b0dae994d933ee721adf414
Nikhil Shukla <nikhilshukla8652@gmail.com>
2025-04-30 14:20:38 +0530

    Fix: use relative path for Vite base config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ✅ THIS is correct for static hosting like Vercel drag-drop or GitHub Pages
+  base: './', // ✅ THIS is correct for static hosting like Vercel drag-drop or GitHub Pages
 });
