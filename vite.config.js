b341cc39a75b09358a8493ec65676d210cff0697
Nikhil Shukla <nikhilshukla8652@gmail.com>
2025-04-30 17:46:07 +0530

    Deploy frontend update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ✅ MATCH karo repo/project name se
+  base: './', // ✅ MATCH karo repo/project name se
 });
